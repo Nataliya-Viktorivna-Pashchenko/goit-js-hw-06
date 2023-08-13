83b348dd428bdc89ca8a3bcd1b71805895e9ac18
Nataliya-Viktorivna-Pashchenko <130203943+Nataliya-Viktorivna-Pashchenko@users.noreply.github.com>
2023-08-13 22:18:20 +0200

    hw_modul6_2

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector("#boxes");
+const inputDiv = document.querySelector("#controls");
+const input = inputDiv.firstElementChild;
+
+console.log(input);
+
+ 
+  function createBoxes(amount) {
+     let boxesArr = [];
+    for (let i = 0; i < amount; i += 1) {
+      const boxesNew = document.createElement("div");
+      boxesNew.style.height = `${30 + 10 * i}px`;
+      boxesNew.style.width = `${30 + 10 * i}px`;
+      boxesNew.style.backgroundColor = `${getRandomHexColor()}`;
+      //boxesEl.insertAdjacentElement("beforeend", boxesNew);
+      boxesArr.push(boxesNew);
+    }
+    return boxesArr;
+};
+
+  
+const makeBoxes = (event) => {
+  const boxesAdd = createBoxes(input.value);
+  boxesEl.append(...boxesAdd);
+};
+
+const destroyBoxes = (event) => {
+  boxesEl.innerHTML = '<div id="boxes"></div>';
+};
+
+btnCreate.addEventListener("click", makeBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
